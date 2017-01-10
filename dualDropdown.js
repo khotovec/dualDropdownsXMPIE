ee757f6d9b80fd7f15dd75e9403704d1a4d730ae
khotovec <tom@Tom-Barnick-3.local>
2017-01-10 16:56:29 -0500

    Initial Code
    Initial code where I’m stuck at

---
+++ dualDropdown.js
@@ -0,0 +1,43 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*jslint devel: true */
+
+
+$(document).ready(function () {
+    "use strict";
+    var codes = ["TEST1", "TEST2"];
+    if ($(".clearingConfigCell")) {
+
+        $('#ctl00_cphMainContent_ctlClearingUserData10005_lblCostCenter').hide();
+        $('#ctl00_cphMainContent_ctlClearingUserData10005_txtCostCenter').hide();
+
+        $(".ClearingConfigCell").append("Please Select Cost Center Code 1: ");
+        if ($(".ClearingConfigCell").length > 0) {
+            $(".ClearingConfigCell").parent().append("<select id='myCCselect' class='clearingConfigCell'/><br><br>");
+            $.each(codes, function (i, val) {
+                $('#myCCselect').append($('<option>', {value: val, text : val }));
+            });
+            $("#myCCselect").change(function () {
+                if ($(this).val() === "Please Select Cost Center Code:") {
+                    $(".ClearingConfigCell").val("#myCCselect1");
+                }
+            });
+        }
+        $(".ClearingConfigCell").append("Please Select Cost Center Code 2: ");
+        if ($(".ClearingConfigCell").length > 0) {
+            $(".ClearingConfigCell").parent().append("<select id='myCCselect2' class='clearingConfigCell'/>");
+            $.each(codes, function (i, val) {
+                $('#myCCselect2').append($('<option>', {value: val, text : val }));
+            });
+            $("#myCCselect2").change(function () {
+                if ($(this).val() === "Please Select Cost Center Code:") {
+                    $(".ClearingConfigCell").val("#myCCselect2");
+                }
+            });
+        }
+    }
+    $(".customCostCenterField").keyup(function () {
+        $("#ctl00_cphMainContent_ctlClearingUserData10005_txtCostCenter").val($("#myCCselect").val() + "||" + $("#myCCselect2").val());
+    });
+});
+
