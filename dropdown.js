9092b0bfc32703983ec54f29fa4eb3bb8c572879
khotovec <tom@Tom-Barnick-3.local>
2017-01-11 11:08:42 -0500

    Change to Hide

--- dropdown.js
+++ dropdown.js
@@ -4,4 +4,10 @@
 
 $(document).ready(function () {
     "use strict";
+    var inputlbl = document.getElementById('inputlbl'),
+        inputtxt = document.getElementById('inputtxt'),
+        codes = ["", "TEST1", "TEST2"];
+    inputlbl.style.display = 'none';
+    inputtxt.style.display = 'none';
+    $(".divTest").append("<div class=Hello World");
 });
