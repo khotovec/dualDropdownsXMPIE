54c249bfe1c1376076276897adb3c668605c4b75
khotovec <tom@Tom-Barnick-3.local>
2017-01-12 17:14:50 -0500

    Creating New Elements
    Creating new elements to replaced the ones I’m blowing out.

--- dropdown.js
+++ dropdown.js
@@ -6,8 +6,9 @@ $(document).ready(function () {
     "use strict";
     var inputlbl = document.getElementById('inputlbl'),
         inputtxt = document.getElementById('inputtxt'),
-        codes = ["", "TEST1", "TEST2"];
+        codes = ["", "TEST1", "TEST2"],
+        newLabel = document.createElement("label"),
+        newInput = document.createElement("input");
     inputlbl.style.display = 'none';
     inputtxt.style.display = 'none';
-    $(".divTest").append("<div class=Hello World");
 });
