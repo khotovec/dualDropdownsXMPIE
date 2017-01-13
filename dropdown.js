30e1abe36e820dcf46895c06350bfa40d5ba5ebf
khotovec <tom@Tom-Barnick-3.local>
2017-01-13 10:52:17 -0500

    Update
    Update

---
+++ dropdown.js
@@ -0,0 +1,16 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+/*jslint devel: true */
+
+$(document).ready(function () {
+    "use strict";
+    var inputlbl = document.getElementById('inputlbl'),
+        inputtxt = document.getElementById('inputtxt'),
+        codes = ["", "TEST1", "TEST2"],
+        newLabel = document.createElement("label"),
+        newInput = document.createElement("input");
+    inputlbl.style.display = 'none';
+    inputtxt.style.display = 'none';
+});
+
+
