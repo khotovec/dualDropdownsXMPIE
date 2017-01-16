b996f4e7a1a9b3f3f43bb17367ae1a7c79f6574a
khotovec <tom@Tom-Barnick-3.local>
2017-01-16 11:40:53 -0500

    Large Update
    Hiding main lbl and txt field and appending the div with new fields.

--- dropdown.js
+++ dropdown.js
@@ -2,15 +2,26 @@
 /*global $, jQuery*/
 /*jslint devel: true */
 
-$(document).ready(function () {
+$(document).ready(function main() {
     "use strict";
-    var inputlbl = document.getElementById('inputlbl'),
+    var mainDiv = document.getElementById('formArea'),
+        inputlbl = document.getElementById('inputlbl'),
         inputtxt = document.getElementById('inputtxt'),
-        codes = ["", "TEST1", "TEST2"],
-        newLabel = document.createElement("label"),
-        newInput = document.createElement("input");
+        newlbl = document.createElement('divNewlbl'),
+        newtxt = document.createElement('divNewtxt'),
+        newlbl2 = document.createElement('divNewlbl2'),
+        newtxt2 = document.createElement('divNewtxt2');
+    newlbl.innerHTML = '<label>Please select your cost center: </label>';
+    newtxt.innerHTML = '<select id="mycc1"> <option>Testing1</option> <option>Dug</option> </select>';
+    newlbl2.innerHTML = '<br><br><label>Please select your cost center: </label>';
+    newtxt2.innerHTML = '<select id="mycc2"><option>Testing1</option><option>Dug</option></select>';
     inputlbl.style.display = 'none';
     inputtxt.style.display = 'none';
+    mainDiv.parentNode.appendChild(newlbl);
+    newlbl.style.color = 'white';
+    mainDiv.parentNode.appendChild(newtxt);
+    mainDiv.parentNode.appendChild(newlbl2);
+    newlbl2.style.color = 'white';
+    mainDiv.parentNode.appendChild(newtxt2);
+    
 });
-
-
